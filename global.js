3ff0b7528a2a1ed22152fe0bc88e41c692c6d8c4
Evo-32 <evodieumuhuza@gmail.com>
2026-04-23 18:26:53 +0200

    export

--- global.js
+++ global.js
@@ -1,10 +1,13 @@
 // console.log(global)
 
-global.setTimeout(() => {
-    console.log('In the timeout')
-    clearInterval(int)
-},3000)
+// global.setTimeout(() => {
+//     console.log('In the timeout')
+//     clearInterval(int)
+// },3000)
 
-const int  = setInterval(() => {
-    console.log('In the interval')
-},1000)
+// const int  = setInterval(() => {
+//     console.log('In the interval')
+// },1000)
+
+// console.log(__dirname)
+// console.log(__filename)
---
+++ people.js
@@ -0,0 +1,5 @@
+const people = ["Alice", "Bob", "Charlie", "Diana", "Ethan"];
+
+console.log(people)
+
+module.exports = people
