e2d0171d8a071f975fe4ccf5120ed6c39e2798e1
Evo-32 <evodieumuhuza@gmail.com>
2026-04-23 18:14:49 +0200

    2 commit

---
+++ global.js
@@ -0,0 +1,10 @@
+// console.log(global)
+
+global.setTimeout(() => {
+    console.log('In the timeout')
+    clearInterval(int)
+},3000)
+
+const int  = setInterval(() => {
+    console.log('In the interval')
+},1000)
