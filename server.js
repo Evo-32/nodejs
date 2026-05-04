ce81cc4167f4ff72fb3da1b2d7cff6c2ddf94119
Evo-32 <evodieumuhuza@gmail.com>
2026-05-04 19:42:30 +0200

    send response as a file

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const http = require('http')
+const fs = require('fs')
 
 const server = http.createServer((req,res) =>{
     console.log(req.url,req.method)
@@ -7,10 +8,21 @@ const server = http.createServer((req,res) =>{
 
     res.setHeader('Content.Type', 'test/hml')
 
-    res.write('<head><link rel= "stylesheet" href= "#"></head>')
-    res.write('<p>Hello dear!</p>');
-    res.write('<p>Helloooo thereeee!</p>');
-    res.end()
+
+    fs.readFile('./views/index.html',(err,data) => {
+        if(err){
+            console.log(err)
+            res.end()
+        }else{
+            //res.write(data)
+            res.end(data)
+        }
+    })
+
+    // res.write('<head><link rel= "stylesheet" href= "#"></head>')
+    // res.write('<p>Hello dear!</p>');
+    // res.write('<p>Helloooo thereeee!</p>');
+    // res.end()
 })
 
 server.listen(3000, 'localhost', () => {
