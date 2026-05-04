16f5e494378acce922a50feea6213003da545532
Evo-32 <evodieumuhuza@gmail.com>
2026-05-04 19:34:04 +0200

    sending an htm response

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 const http = require('http')
 
 const server = http.createServer((req,res) =>{
-    console.log('request made')
+    console.log(req.url,req.method)
+
+    //set header content type
+
+    res.setHeader('Content.Type', 'test/hml')
+
+    res.write('<head><link rel= "stylesheet" href= "#"></head>')
+    res.write('<p>Hello dear!</p>');
+    res.write('<p>Helloooo thereeee!</p>');
+    res.end()
 })
 
 server.listen(3000, 'localhost', () => {
