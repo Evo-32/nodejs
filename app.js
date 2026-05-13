d98c489ce9e740bd7d2915550d8925c8680c104e
Evo-32 <evodieumuhuza@gmail.com>
2026-05-13 11:30:24 +0200

    routing

--- app.js
+++ app.js
@@ -9,7 +9,11 @@ const app = express()
 app.listen(3000);
 
 app.get('/', (req,res) => {
+    // res.send('<p>Home page</p>')
+    res.sendFile('./views/index.html', {root: __dirname})
+})
 
-    res.send('<p>Home page</p>')
-
+app.get('/about', (req,res) => {
+    // res.send('<p>About page</p>')
+    res.sendFile('./views/about.html', {root: __dirname})
 })
