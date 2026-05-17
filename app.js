dc14be9bf8500c991ac6ace15414ffea62a20e2c
Evo-32 <evodieumuhuza@gmail.com>
2026-05-17 10:53:46 +0200

    The view engine

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const express  = require('express')
 
 const app = express()
 
-
+//register view engine
+app.set('view engine', 'ejs');
 
 //listen for requests
 
@@ -10,10 +11,28 @@ app.listen(3000);
 
 app.get('/', (req,res) => {
     // res.send('<p>Home page</p>')
-    res.sendFile('./views/index.html', {root: __dirname})
+    // res.sendFile('./views/index.html', {root: __dirname})
+    res.render('index')
 })
 
 app.get('/about', (req,res) => {
     // res.send('<p>About page</p>')
-    res.sendFile('./views/about.html', {root: __dirname})
+    // res.sendFile('./views/about.html', {root: __dirname})
+    res.render('about')
+})
+
+//redirect
+// app.get('/about-us', (req,res) =>{
+//     res.redirect('/about');
+// })
+
+app.get("/blog/create",(req,res) => {
+    res.render('create')
+})
+
+//404 page
+
+app.use((req,res) => {
+    // res.status(404).sendFile('./views/404.html', {root: __dirname})
+    res.status(404).render('404');
 })
