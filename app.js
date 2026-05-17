dca1eaf738d5dcd85b7bbd3d93150b0ec6bc64a0
Evo-32 <evodieumuhuza@gmail.com>
2026-05-17 11:39:31 +0200

    loop in blogs using ejs

--- app.js
+++ app.js
@@ -1,38 +1,53 @@
-const express  = require('express')
+const express = require("express");
 
-const app = express()
+const app = express();
 
 //register view engine
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
 //listen for requests
 
 app.listen(3000);
 
-app.get('/', (req,res) => {
-    // res.send('<p>Home page</p>')
-    // res.sendFile('./views/index.html', {root: __dirname})
-    res.render('index')
-})
+app.get("/", (req, res) => {
 
-app.get('/about', (req,res) => {
-    // res.send('<p>About page</p>')
-    // res.sendFile('./views/about.html', {root: __dirname})
-    res.render('about')
-})
+    const blogs = [
+  {
+    title: "Yoshi finds eggs",
+    snippet: "Lorem ipsum dolor sit amet consectetur",
+  },
+  {
+    title: "Mario finds stars",
+    snippet: "Lorem ipsum dolor sit amet consectetur",
+  },
+  {
+    title: "How to defeat bowser",
+    snippet: "Lorem ipsum dolor sit amet consectetur",
+  },
+];
+  // res.send('<p>Home page</p>')
+  // res.sendFile('./views/index.html', {root: __dirname})
+  res.render("index", { title: "Home", blogs });
+});
+
+app.get("/about", (req, res) => {
+  // res.send('<p>About page</p>')
+  // res.sendFile('./views/about.html', {root: __dirname})
+  res.render("about", { title: "About" });
+});
 
 //redirect
 // app.get('/about-us', (req,res) =>{
 //     res.redirect('/about');
 // })
 
-app.get("/blog/create",(req,res) => {
-    res.render('create')
-})
+app.get("/create", (req, res) => {
+  res.render("create", { title: "Create a blog" });
+});
 
 //404 page
 
-app.use((req,res) => {
-    // res.status(404).sendFile('./views/404.html', {root: __dirname})
-    res.status(404).render('404');
-})
+app.use((req, res) => {
+  // res.status(404).sendFile('./views/404.html', {root: __dirname})
+  res.status(404).render("404", { title: "404" });
+});
